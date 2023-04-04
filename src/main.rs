2d05201ff479247eba42d6d5704fdda69a462185
AJ Dampil <127729583+Diurem@users.noreply.github.com>
2023-04-04 02:24:35 -0700

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#[macro_use] extern crate rocket;
+
+#[get("/add")]
+fn add() -> &'static str {
+    "Hey! Let's add!"
+}
+
+#[get("/add/<a>")]
+fn add_a(a: u32) -> String {
+    format!("Hey! Let's add! Your first input is here: {} but you haven't put another number <b>", a)
+}
+
+#[get("/add/<a>/<b>")]
+fn add_a_b(a: u32, b: u32) -> String {
+    format!("Hey! Let's add! Your inputs are: {} and {}. The sum is {}!", a, b, a + b)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![add, add_a, add_a_b])
+}
